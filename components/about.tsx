8bb92857f75becd1c4f761f1ff10fc7995774df0
Valeriy Zhirnov <neonailol@gmail.com>
2023-01-10 15:03:47 +0300

    Fixed ring in production

--- components/about.tsx
+++ components/about.tsx
@@ -8,7 +8,7 @@ export default function About() {
 	return (
 		<>
 			<section className={styles.about}>
-				<Image src="/images/profile.jpg" priority alt={about.name} width={512} height={512} className="dark:ring-gray-50" />
+				<Image src="/images/profile.jpg" priority alt={about.name} width={512} height={512} className="dark:outline-gray-50" />
 				<h2>{about.title}</h2>
 				{about.social.map(social => <a key={social.link} href={social.link}><Icon icon={SocialIcon(social.name)} /></a>)}
 			</section>
