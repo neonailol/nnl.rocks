1a297fbf167f7445cfe6087667c113e4acdf6d8a
Valeriy Zhirnov <neonailol@gmail.com>
2023-03-13 17:45:19 +0300

    Added some GAN videos

--- data/bookmarks/videos.tsx
+++ data/bookmarks/videos.tsx
@@ -21,6 +21,36 @@ export const videos: Category = {
 			link: 'https://youtu.be/slTEz6555Ts',
 			date: '2020-09-19',
 			tags: ['Programming', 'GameDev', 'Procedural Generation'],
-		}
+		},
+		{
+			title: 'Diffusion models from scratch in PyTorch',
+			link: 'https://youtu.be/a4Yfz2FxXiY',
+			date: '2022-07-18',
+			tags: ['PyTorch', 'Machine Learning', 'Deep Learning', 'Generative Adversarial Network', 'Diffusion'],
+		},
+		{
+			title: 'Diffusion Models | Paper Explanation | Math Explained',
+			link: 'https://youtu.be/HoKDTa5jHvg',
+			date: '2022-06-06',
+			tags: ['Machine Learning', 'Deep Learning', 'Generative Adversarial Network', 'Diffusion'],
+		},
+		{
+			title: 'Diffusion Models | PyTorch Implementation',
+			link: 'https://youtu.be/TBCRlnwJtZU',
+			date: '2022-09-21',
+			tags: ['PyTorch', 'Machine Learning', 'Deep Learning', 'Generative Adversarial Network', 'Diffusion'],
+		},
+		{
+			title: 'VQ-GAN | Paper Explanation',
+			link: 'https://youtu.be/wcqLFDXaDO8',
+			date: '2022-02-16',
+			tags: ['Machine Learning', 'Deep Learning', 'Generative Adversarial Network', 'Vector Quantized'],
+		},
+		{
+			title: 'VQ-GAN | PyTorch Implementation',
+			link: 'https://youtu.be/_Br5WRwUz_U',
+			date: '2022-02-16',
+			tags: ['PyTorch', 'Machine Learning', 'Deep Learning', 'Generative Adversarial Network', 'Vector Quantized'],
+		},
 	]
 }
