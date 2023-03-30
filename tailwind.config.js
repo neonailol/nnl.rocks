628d5f9839288218f3f0a6a188ad93eb769ed675
Valeriy Zhirnov <neonailol@gmail.com>
2023-03-30 22:45:23 +0300

    Dark mode not working again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-		'./app/**/*.{js,ts,jsx,tsx}',
+		'./styles/**/*.css',
+		'./pages/**/*.{js,ts,jsx,tsx,css,module.css}',
+		'./components/**/*.{js,ts,jsx,tsx,css,module.css}',
+		'./app/**/*.{js,ts,jsx,tsx,css,module.css}',
 	],
 	darkMode: 'class',
 	theme: {
